80e1e5fe7db4de2a613a33c605f839ff6270e694
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-04-08 22:17:04 +0600

    try catch use

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
-const getAllFromDB  = async (req: Request, res:Response) => {
-    const result = await AdminService.getAllFromDB(req.query)
+const getAllFromDB = async (req: Request, res: Response) => {
+  try {
+    const result = await AdminService.getAllFromDB(req.query);
     res.status(200).json({
-        success: true,
-        message: "Admin data fetched",
-        data: result
-    })
-}
-
+      success: true,
+      message: "Admin data fetched",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.name || "Southing went wrong",
+      error: error,
+    });
+  }
+};
 
 export const AdminController = {
-    getAllFromDB
-}
+  getAllFromDB,
+};
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from "express";
 import { UserService } from "./user.service";
 
-const createAdmin  = async (req: Request, res:Response) => {
-    
-    const result = await UserService.createAdmin(req.body)
-    res.send(result)
-}
+const createAdmin = async (req: Request, res: Response) => {
+  try {
+    const result = await UserService.createAdmin(req.body);
+    res.status(200).json({
+        success: true,
+        message: "Admin created successfully",
+        data: result
+    })
 
+  } catch (error: any) {
+    res.status(500).json({
+        success: false,
+        message: error.name || "Southing went wrong",
+        error: error
+    })
+  }
+};
 
 export const UserController = {
-    createAdmin
-}
+  createAdmin,
+};

ddd66798d7588b2d0129e5787b92d3fb50c3a041
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-04-12 22:56:33 +0600

    59-1 Reduce Try-Catch Redundancy

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,12 +1,22 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterPikedFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 
-const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+export const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next();
+    }
+  };
+};
+
+const getAllFromDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
     const filters = pick(req.query, adminFilterPikedFields);
     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
     const result = await AdminService.getAllFromDB(filters, options);
@@ -23,15 +33,12 @@ const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => 
       meta: result.meta,
       data: result.data,
     });
-  } catch (error: any) {
-    next()
   }
-};
+);
 
-const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
-
-  try {
+const getByIdFromDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
     const result = await AdminService.getByIdFromDB(id);
     sendResponse(res, {
       statusCode: httpStatus.OK,
@@ -39,15 +46,13 @@ const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) =>
       message: "Admin data fetched by id",
       data: result,
     });
-  } catch (error: any) {
-    next(error)
   }
-};
+);
 
-const updateIntoDB = async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
+const updateIntoDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
     const result = await AdminService.updateIntoDB(id, req.body);
     sendResponse(res, {
       statusCode: httpStatus.OK,
@@ -55,15 +60,13 @@ const updateIntoDB = async (req: Request, res: Response, next: NextFunction) => 
       message: "Admin data updated!",
       data: result,
     });
-  } catch (error: any) {
-    next(error)
   }
-};
+);
 
-const deleteFromDB = async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
+const deleteFromDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
     const result = await AdminService.deleteFromDB(id);
 
     sendResponse(res, {
@@ -72,15 +75,13 @@ const deleteFromDB = async (req: Request, res: Response, next: NextFunction) => 
       message: "Admin data deleted!",
       data: result,
     });
-  } catch (error: any) {
-    next(error)
   }
-};
+);
 
-const softDeleteFromDB = async (req: Request, res: Response, next: NextFunction) => {
-  const { id } = req.params;
+const softDeleteFromDB: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  try {
     const result = await AdminService.softDeleteFromDB(id);
 
     sendResponse(res, {
@@ -89,10 +90,8 @@ const softDeleteFromDB = async (req: Request, res: Response, next: NextFunction)
       message: "Admin soft deleted!",
       data: result,
     });
-  } catch (error: any) {
-    next(error)
   }
-};
+);
 
 export const AdminController = {
   getAllFromDB,
--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,23 +1,20 @@
 import { Request, Response } from "express";
 import { UserService } from "./user.service";
+import { catchAsync } from "../Admin/admin.controller";
+import sendResponse from "../../../shared/sendResponse";
+import httpStatus from "http-status";
 
-const createAdmin = async (req: Request, res: Response) => {
-  try {
+
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
     const result = await UserService.createAdmin(req.body);
-    res.status(200).json({
-        success: true,
-        message: "Admin created successfully",
-        data: result
-    })
-
-  } catch (error: any) {
-    res.status(500).json({
-        success: false,
-        message: error.name || "Southing went wrong",
-        error: error
-    })
-  }
-};
+    
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Admin created successfully",
+      data: result,
+    });
+})
 
 export const UserController = {
   createAdmin,
